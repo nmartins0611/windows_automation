287d0791a4cc46d0032567fc1bed9889db8f87dc
nmartins0611 <nmartins@redhat.com>
2024-02-28 20:55:16 +0200

    Update iis_powershell.ps1

--- iis_powershell.ps1
+++ iis_powershell.ps1
@@ -1,44 +1,37 @@
-# Variables
-$remoteServer = "your_remote_server_hostname_or_IP"
-$adminUsername = "your_admin_username"
-$adminPassword = "your_admin_password"
-$certThumbprint = "your_SSL_certificate_thumbprint"
-$websiteName = "SampleWebsite"
+# Define variables
+$remoteServer = "your_remote_server_address"
+$username = "your_username"
+$password = "your_password"
 $webpageContent = @"
 <html>
 <head>
-    <title>Sample Web Page</title>
+    <title>Simple Web Page</title>
 </head>
 <body>
-    <h1>Welcome to the Sample Website!</h1>
-    <p>This is a sample webpage hosted on IIS.</p>
+    <h1>Hello, World!</h1>
+    <p>This is a simple webpage hosted on port 8080.</p>
 </body>
 </html>
 "@
 
-# Set up WinRM for HTTPS communication
-$winrmConfig = @{
-    CertificateThumbprint = $certThumbprint
-    Port = 5986
-    Force = $true
-}
-Invoke-Command -ComputerName $remoteServer -ScriptBlock {
-    Enable-PSRemoting -Force
-    Set-WSManQuickConfig -UseSSL -Force
-    Register-PSSessionConfiguration -Name "PowerShell" @PSBoundParameters -Force
-} -Credential (Get-Credential -Credential $adminUsername)
+# Ignore certificate errors
+$sessionOption = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
 
-# Establish a remote PowerShell session
-$session = New-PSSession -ComputerName $remoteServer -Credential (Get-Credential -Credential $adminUsername)
-Invoke-Command -Session $session -ScriptBlock {
-    Import-Module WebAdministration
-    # Create a new website
-    New-Website -Name $using:websiteName -Port 8080 -PhysicalPath "C:\inetpub\wwwroot\$using:websiteName"
-    # Create a new index.html file for the website
-    Set-Content -Path "C:\inetpub\wwwroot\$using:websiteName\index.html" -Value $using:webpageContent
-    # Start the website
-    Start-Website -Name $using:websiteName
+# Create a PSSession to the remote server
+$remoteSession = New-PSSession -ComputerName $remoteServer -Credential (Get-Credential -Credential $username) -SessionOption $sessionOption
+
+# Script block to execute on the remote server
+$scriptBlock = {
+    # Install IIS
+    Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+
+    # Create a simple HTML file
+    $webpagePath = "C:\inetpub\wwwroot\index.html"
+    Set-Content -Path $webpagePath -Value $using:webpageContent -Force
 }
 
-# Clean up the session
-Remove-PSSession $session
+# Invoke the script block on the remote server
+Invoke-Command -Session $remoteSession -ScriptBlock $scriptBlock
+
+# Close the remote session
+Remove-PSSession $remoteSession
